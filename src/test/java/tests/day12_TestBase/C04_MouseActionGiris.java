7724590801e2bbe595f97aaa1158f4640fcc8f5c
Kazım Nihat Tekiner <nihattekiner@gmail.com>
2021-06-07 23:21:25 +0300

    update

---
+++ src/test/java/tests/day12_TestBase/C04_MouseActionGiris.java
@@ -0,0 +1,12 @@
+package tests.day12_TestBase;
+
+import org.testng.annotations.Test;
+import utilities.TestBaseClass;
+
+public class C04_MouseActionGiris extends TestBaseClass {
+
+    @Test
+    public void test() {
+
+    }
+}
