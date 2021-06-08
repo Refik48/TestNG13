4c62624df4bc36832cf4bee794dcbefd80967003
Kazım Nihat Tekiner <nihattekiner@gmail.com>
2021-06-08 20:35:27 +0300

    update

--- src/test/java/tests/day12_TestBase/C04_MouseActionGiris.java
+++ src/test/java/tests/day12_TestBase/C04_MouseActionGiris.java
@@ -1,5 +1,6 @@
 package tests.day12_TestBase;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import utilities.TestBaseClass;
 
@@ -7,6 +8,7 @@ public class C04_MouseActionGiris extends TestBaseClass {
 
     @Test
     public void test() {
-
+        driver.get("https://www.amazon.com");
+        driver.findElement(By.xpath("//span[text()='Create a List']")).click();
     }
 }
